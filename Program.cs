da8eabab67d18e01c94bd040f18180bfe7e8d559
SethRodgerPatterson <000915695@mohawkcollege.ca>
2023-11-17 10:29:04 -0500

    Basics ++
    Screen Size
    Player Drawing (Basics)
    Player Controls (Basics)
    Simple Gravity
    Anti- Fall out of map collision

--- Program.cs
+++ Program.cs
@@ -4,17 +4,21 @@ using System.Numerics;
 
 namespace Assignment_4_Group_2D_Game_Project
 {
-
+    
+    
 
     internal class Program
     {
-        
-        static string title = "Game Title";
+        static int WindowWidth = 1200;
+        static int WindowHeight = 800;
+
+        static Vector2 PlayerPosition { get; set; } = new Vector2();
+        static Vector2 PlayerSize = new Vector2(50,50);
 
         static void Main(string[] args)
         {
             // Create a window to draw to. The arguments define width and height
-            Raylib.InitWindow(800, 600, title);
+            Raylib.InitWindow(WindowWidth, WindowHeight, "Assignment-2D-Game-Project");
             
             Raylib.SetTargetFPS(60);
 
@@ -31,8 +35,9 @@ namespace Assignment_4_Group_2D_Game_Project
 
                 
                 Update();
+                Player();
 
-               
+
                 Raylib.EndDrawing();  // Stop drawing to the canvas, begin displaying the frame
             }
             
@@ -42,6 +47,38 @@ namespace Assignment_4_Group_2D_Game_Project
         static void Setup() // Your one-time setup code here
         {
             
+        }
+
+
+        static void Player()
+        {
+            Vector2 MoveLeft = new Vector2(-5, 0);
+            Vector2 MoveRight = new Vector2(5, 0);
+            Vector2 GravityBasic = new Vector2(0, 5);
+            float PlayerBottomCorner = PlayerPosition.X + PlayerPosition.Y;
+
+            //Draws Player
+            Raylib.DrawRectangle((int)PlayerPosition.X, (int)PlayerPosition.Y, (int)PlayerSize.X, (int)PlayerSize.Y, Color.BLUE);
+        
+            // Controls
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_A) || Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+            {
+                PlayerPosition = PlayerPosition + MoveLeft;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_D) || Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+            {
+                PlayerPosition = PlayerPosition + MoveRight;
+            }
+
+
+            //Collison with Bottom (BASIC)
+            if (PlayerBottomCorner < WindowHeight || PlayerBottomCorner == WindowHeight)
+            {
+                PlayerPosition = PlayerPosition + GravityBasic;
+            }
+           
+
         }
 
         static void Update() // Your game code run each frame here
